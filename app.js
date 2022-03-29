8fd23cd2a790444865ee54442e3ab0a5a229ae85
Jinang Shah <jinang.shah@solutionanalysts.in>
2022-03-29 17:52:00 +0530

    dev commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const url = "https://fakestoreapi.com/products"
 
 request({url:url, json:true},(error, response)=>{
     if(error){
-        console.log('Opps!! Something went wrong');
+        console.log('Opps!! Something went wrong')
     }
     else if(response.body.error){
         console.log('unable to find product')
